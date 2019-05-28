ef077d72f5da1663ba74de30a052e666f73f7f5e
mdskrzypczyk <mdskrzypczyk@tuta.io>
2019-05-28 09:36:00 +0200

    Removed unused code

--- src/bb84_purified/runBB84.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-ps aux | grep python | grep "bb84_purified" | awk {'print $2'} | xargs kill -9
-python bb84_purified/aliceBB84.py &
-python bb84_purified/eveBB84.py &
-python bb84_purified/bobBB84.py
